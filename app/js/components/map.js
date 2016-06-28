8b2d9ce6318b84f492f205df9325031174df3b60
Allen Elks <allen.elks@gmail.com>
2016-06-28 07:20:42 -0400

    default for heatmap component

--- app/js/components/map.js
+++ app/js/components/map.js
@@ -3,8 +3,11 @@
 import React, {Component} from 'react';
 import {Map, Marker, Popup, TileLayer} from 'react-leaflet';
 import HeatmapLayer from 'react-leaflet-heatmap-layer';
+
 const position = [42.87, -97.38];
 const styles = {height: '90vh'};
+const ZOOM_THRESHOLD = 8;
+const INITIAL_ZOOM = 4;
 
 export default class HeatMap extends Component {
 
@@ -23,26 +26,17 @@ export default class HeatMap extends Component {
   render() {
     const {points} = this.props;
     console.log("Now showing " + points.length + " points");
-    let markers = (this.state.zoom >= 8) ? renderMarkers(points) : null;
+    let markers = (this.state.zoom >= ZOOM_THRESHOLD) ? renderMarkers(points) : null;
+    let heatmap = (this.state.zoom < ZOOM_THRESHOLD)  ? renderHeatMap(points) : renderHeatMap([]);
+
     return (
       <div style={styles}>
-        <Map center={position} zoom={4} style={styles} onMoveend={this.handleMove}>
-          <HeatmapLayer
-            max={0.1}
-            radius={0.1}
-            minOpacity={0.3}
-            blur={0}
-            maxZoom={9}
-            gradient={{1.0  : 'red'}}
-            points={this.props.points}
-            longitudeExtractor={m => m[1]}
-            latitudeExtractor={m => m[0]}
-            intensityExtractor={m => parseFloat(m[2])}/>
+        <Map center={position} zoom={INITIAL_ZOOM} style={styles} onMoveend={this.handleMove}>
           {markers}
+          {heatmap}
           <TileLayer
             url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          />
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
         </Map>
         This product includes GeoLite2 data created by MaxMind, available from
         <a href="http://www.maxmind.com">http://www.maxmind.com</a>.
@@ -87,13 +81,20 @@ const renderMarkers = (points) => {
   });
 };
 
-const renderList = (ips) => {
-  console.log(ips);
-   return (
-     <ul>
-      ips.map((ipv6, index) => {
-        <li key={index}>{ipv6}</li>
-      })
-    </ul>
-   );
-};
+const renderHeatMap = (points, options = {max: 0.1, radius: 0.1, minOpacity: 0.3, blur: 0, maxZoom: 9}) => {
+  let {max, radius, minOpacity, blur, maxZoom} = options;
+
+  return (
+    <HeatmapLayer
+      max={max}
+      radius={radius}
+      minOpacity={minOpacity}
+      blur={blur}
+      maxZoom={maxZoom}
+      gradient={{1.0  : 'red'}}
+      points={points}
+      longitudeExtractor={m => m[1]}
+      latitudeExtractor={m => m[0]}
+      intensityExtractor={m => parseFloat(m[2])}/>
+  );
+}
